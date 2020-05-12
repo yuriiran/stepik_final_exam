f7bb35ea70be9c33e78d199d5e74ce50e96c94c5
John Doe <yurii01031989@gmail.com>
2020-05-12 15:20:56 +0800

    last update

---
+++ pages/main_page.py
@@ -0,0 +1,7 @@
+from stepik_final_exam.pages.base_page import BasePage
+from selenium.webdriver.common.by import By
+
+class MainPage(BasePage): 
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link.click()
