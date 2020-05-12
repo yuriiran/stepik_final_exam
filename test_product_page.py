9c4a46f5876642d76134957d55e7fe2c299ac8dd
John Doe <yurii01031989@gmail.com>
2020-05-12 22:24:05 +0800

    4.3.5 amend

--- test_product_page.py
+++ test_product_page.py
@@ -1,14 +1,30 @@
 from stepik_final_exam.pages.product_page import ProductPage
 import time
+import pytest
+
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9",
+                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+                                  "okay_link"])
+
+def test_guest_can_add_product_to_basket(browser, link):
+        # link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+        product_page = ProductPage(browser, link)
+        product_page.open() # откртыие страницы
+        product_page.add_to_basket()  # добавление товара
+        product_page.solve_quiz_and_get_code() # решение задачи в сплывающем окне
+        product_page.should_be_book_name() # проверка названия книги
+        product_page.should_be_book_price() # проверка цены книги
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    product_page = ProductPage(browser, link)
-    product_page.open() # откртыие страницы
-    product_page.add_to_basket()  # добавление товара
-    product_page.solve_quiz_and_get_code() # решение задачи в сплывающем окне
-    product_page.should_be_book_name() # проверка названия книги
-    product_page.should_be_book_price() # проверка цены книги
-
-
+# @pytest.mark.parametrize('link', ["okay_link",
+#                                   pytest.param("bugged_link", marks=pytest.mark.xfail),
+#                                   "okay_link"])
